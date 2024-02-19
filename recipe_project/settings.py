1dedd2ffaa6fee9a02e22075aec4273cf1dc4266
Tinkjimmy <tinkjimmy938@gmail.com>
2024-02-19 18:33:29 +0100

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -119,6 +119,13 @@ LOGIN_URL='/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+
+]
+
+# The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
